b7ebe19494b079a6bb43f9079c23711e1b2bb707
rnann <rnannigm@gmail.com>
2018-09-26 15:58:41 -0300

    Update r-utils.r

--- lib/r-utils.r
+++ lib/r-utils.r
@@ -1,11 +1,27 @@
 #!/usr/bin/env Rscript
 
+##
+# Returns the first value that is not null
+#
+# @param x  Left value
+# @param y  Right value
+#
+# @return   The first value that is not null
+#
 `%??%` <- function(x, y) {
     if (!is.null(x)) x else y
 }
 
+##
+# Check if the element belongs to the specified
+#
+# @param x  Left value
+# @param y  Right value
+#
+# @return   TRUE if the element belongs to the specified
+#
 `%is%` <- function(x, y) {
-    is(x, y)
+    inherits(x, y)
 }
 
 `%if%` <- function(x, y) {
